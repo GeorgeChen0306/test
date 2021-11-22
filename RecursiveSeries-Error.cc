286e3579ac25c81f2ec9decbab613fc640519ccb
GeorgeChen0306 <georgechen0306@gmail.com>
2021-11-22 15:32:12 -0500

    Add a .cc file

---
+++ RecursiveSeries-Error.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+
+/**
+ *  Compute the sum of the geometric series: 
+ *     sum_{k=0}^{n-1} (ar^k)
+ *  Where a, r are real values, and n is an integer.
+ *  https://www.mathsisfun.com/algebra/sequences-sums-geometric.html
+ **/
+double GeometricSum(double a, double r, int n) {
+  return a*pow(r,n-1) + GeometricSum(a,r,n-1);
+}
+
+
+int main() {
+  double a = 1.0;
+  double r = 0.5;
+  int    n = 0;
+
+  cout << endl << "Computing geometric series ar^k" << endl;
+  cout << "  What is a? ";
+  cin >> a;
+  cout << "  What is r? ";
+  cin >> r;
+  cout << "  How many interations? ";
+  cin >> n;
+  cout << endl;
+  
+  cout << "The sum of that series is: " << GeometricSum(a,r,n) << endl;
+
+  return 0;
+}
+  
